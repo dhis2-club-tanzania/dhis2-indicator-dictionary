a5a920446721778456b72f3e166ecca4bfb80152
josephatJ <josephatjulius24@gmail.com>
2022-07-07 14:01:54 +0300

    refactor: change functions initialization function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
 import { Fn } from '@iapps/function-analytics';
+import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { Fn } from '@iapps/function-analytics';
 export class AppComponent {
   constructor(
     private translate: TranslateService,
-    private titleService: Title
+    private titleService: Title,
+    private httpClient: NgxDhis2HttpClientService
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
     this.translate.setDefaultLang('en');
@@ -22,11 +24,13 @@ export class AppComponent {
     // Set application title
     this.setTitle('Indicator Dictionary');
 
-    if (Fn) {
-      Fn.init({
-        baseUrl: '../../../',
-      });
-    }
+    this.httpClient.rootUrl().subscribe((rootUrl) => {
+      if (Fn) {
+        Fn.init({
+          baseUrl: `${rootUrl}api/`,
+        });
+      }
+    });
   }
 
   public setTitle(newTitle: string) {
